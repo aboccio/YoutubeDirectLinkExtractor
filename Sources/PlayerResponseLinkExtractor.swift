6823939b6074dfa64d373e023679d7669773e7a1
thermogl <info@thermoglobalnuclearwar.com>
2020-01-17 15:31:02 +0000

    use formats instead of adaptive formats

--- Sources/PlayerResponseLinkExtractor.swift
+++ Sources/PlayerResponseLinkExtractor.swift
@@ -6,7 +6,7 @@ enum PlayerResponseLinkExtractor {
         case badDataFormat
         case responseTypeError
         case missingStreamingData
-        case missingAdaptiveFormats
+        case missingFormats
 
         var errorDescription: String? {
             switch self {
@@ -16,18 +16,17 @@ enum PlayerResponseLinkExtractor {
                 return "Unable to parse player response"
             case .missingStreamingData:
                 return "Unable to parse streaming data"
-            case .missingAdaptiveFormats:
-                return "Unable to parse adaptive formats"
+            case .missingFormats:
+                return "Unable to parse formats"
             }
         }
     }
 
     private enum Key {
         static let streamingData = "streamingData"
-        static let adaptiveFormats = "adaptiveFormats"
+        static let formats = "formats"
         static let quality = "quality"
         static let url = "url"
-        static let mimeType = "mimeType"
     }
 
     // Return type to match VideoInfo rawInfo
@@ -35,11 +34,9 @@ enum PlayerResponseLinkExtractor {
         guard let data = playerResponse.data(using: .utf8) else { throw Error.badDataFormat }
         guard let object = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else { throw Error.responseTypeError }
         guard let streamingData = object[Key.streamingData] as? [String: Any] else { throw Error.missingStreamingData }
-        guard let adaptiveFormats = streamingData[Key.adaptiveFormats] as? [[String: Any]] else { throw Error.missingAdaptiveFormats }
-        return adaptiveFormats.compactMap { format -> [String: String]? in
-            guard let quality = format[Key.quality] as? String,
-                let url = format[Key.url] as? String,
-                let mimeType = format[Key.mimeType] as? String, mimeType.contains("video/mp4") else { return nil }
+        guard let formats = streamingData[Key.formats] as? [[String: Any]] else { throw Error.missingFormats }
+        return formats.compactMap { format -> [String: String]? in
+            guard let quality = format[Key.quality] as? String, let url = format[Key.url] as? String else { return nil }
             return [
                 Key.quality: quality,
                 Key.url: url
